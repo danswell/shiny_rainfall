af60706fd810676cab71151e14a99f9e6f14bd92
danswell <danswell.starrs@anu.edu.au>
2020-01-09 15:58:16 +1100

    updates based on suggestions from others

--- App/App.R
+++ App/App.R
@@ -20,7 +20,7 @@ library(leaflet)
 
 #2. Read in static data
 
-Static_data <- read.csv("rainfall_data.csv", header = T)
+Static_data <- read.csv("./App/rainfall_data.csv", header = T)
 Static_data$Value <- as.numeric(Static_data$Value)
 Static_data$DatetimeAEST <- as.POSIXct(Static_data$DatetimeAEST, format = "%Y-%m-%dT%H:%M:%S")
 Static_data$DatetimeAEST <- as_date(Static_data$DatetimeAEST)
@@ -84,7 +84,7 @@ ui <- fluidPage(titlePanel("ACT Rainfall Explorer"),
                                 selected = "410776"),
                     
                     # Select date range to be plotted
-                    sliderInput("Date", strong("Date range"), min = as.Date("1980-01-01"), max = as.Date("2016-12-31"),
+                    sliderInput("Date", strong("Date range"), min = min(Rainfall_data$DatetimeAEST), max = max(Rainfall_data$DatetimeAEST)),
                                 value = c(as.Date("1980-01-01"), as.Date("2016-12-31")),
                                 timeFormat = "%Y-%m-%d"),
                     
@@ -104,12 +104,12 @@ ui <- fluidPage(titlePanel("ACT Rainfall Explorer"),
                     plotOutput(outputId = "lineplot", height = "400px"),
                     #textOutput(outputId = "cumplot", height = "400px"),
                     tags$body("Clicking on pin on map can select site, as can selecting from dropdown menu. Data provided by ACT Government (www.data.act.gov.au). Questions and comments can be directed to
-                    danswell(dot)starrs(at)act(dot)gov(dot)au. Data can be aggregated to monthly or calendar year. Note this is 
+                    danswell(dot)starrs(at)act(dot)gov(dot)au. Data can be aggregated to monthly or calendar year. Aggregation method is summation. Note this is 
                     sensitive to the time slider input - partial months and years will be computed based on time slider. Code for this app can be found at
                               https://github.com/danswell/shiny_rainfall")
                     )
-                    )
                   )
+                
 
 
 #6// Define server function
@@ -127,7 +127,7 @@ server <- function(input, output, session) {
   #Update slider input to reflect site selected
   observeEvent(input$site, {
   updateSliderInput(session, "Date", value = c(min(selected_rain()$DatetimeAEST), max(selected_rain()$DatetimeAEST)),
-                    min = min(selected_rain()$DatetimeAEST), max = max(selected_rain()$DatetimeAEST))
+                    min = min(Rainfall_data$DatetimeAEST), max = max(Rainfall_data$DatetimeAEST))
 })
   
   #Update selected site based on map click
@@ -166,6 +166,8 @@ server <- function(input, output, session) {
       summarise(Yearly_rain = sum(Value, na.rm = T)) %>%
       ggplot() + 
       geom_col(mapping = aes(Year, Yearly_rain), color = "blue") +
+      geom_hline(aes(yintercept = mean(Yearly_rain)), color = "red") +
+      geom_text(aes(min(Year),mean(Yearly_rain),label = round(mean(Yearly_rain),2), vjust = -1)) + 
       labs(x = "Date", y = "Annual Rainfall (mm)", title = paste0("Rainfall at ", input$site))
   }
    else if(input$monthly == TRUE){
@@ -175,11 +177,15 @@ server <- function(input, output, session) {
      mutate(My_date = as.Date(paste(sprintf("%d-%02d", Year, Month), "-01", sep=""))) %>%
      ggplot() + 
      geom_col(mapping = aes(My_date, Monthly_rain), color = "blue") +
+     geom_hline(aes(yintercept = mean(Monthly_rain)), color = "red") +
+     geom_text(aes(min(My_date),mean(Monthly_rain),label = round(mean(Monthly_rain),2), vjust = -1)) + 
      labs(x = "Date", y = "Monthly Rainfall (mm)", title = paste0("Rainfall at ", input$site))
     }
     else {selected_rain2() %>%
        ggplot() +
        geom_col(aes(DatetimeAEST, Value), color = "blue") +
+       geom_hline(aes(yintercept = mean(Value)), color = "red") + 
+       geom_text(aes(min(DatetimeAEST),mean(Value),label = round(mean(Value),2), vjust = -1)) + 
        labs(x = "Date", y = "Daily Rainfall (mm)", title = paste0("Rainfall at ", input$site)) 
      }
   })
